68fb33770ed53a36f332a6c9f5573b5afbccf0ad
Carlos Torres <cctc2013@gmail.com>
2023-01-23 23:34:00 -0300

    Correccion de identacion

--- components/becas/Becas.jsx
+++ components/becas/Becas.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import CardBeca from '../card-beca/CardBeca'
 import ModalBeca from '../modal-beca/ModalBeca'
-import toast, {Toaster} from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import Url from '../../common/Url';
 
 const Becas = () => {
@@ -10,41 +10,41 @@ const Becas = () => {
     const [toastLoading, setToastLoading] = useState(null);
     const [becaSelecionada, setBecaSeleccionada] = useState(null);
     const isMounted = useRef(true);
-    
+
 
     useEffect(() => {
-        return() => {
+        return () => {
             isMounted.current = false;
         }
     }, [])
 
     useEffect(() => {
-        if(isMounted.current){
+        if (isMounted.current) {
             const data = JSON.parse(localStorage.getItem('token'));
             setToastLoading(toast.loading("cargando..."));
             const header = new Headers();
             header.append("Authorization", `bearer ${data.token}`);
             header.append("Content-Type", "application/json");
-      
+
             const requestOption = {
-              method :'GET',
-              headers : header,
-              redirect :'follow'
+                method: 'GET',
+                headers: header,
+                redirect: 'follow'
             }
-      
+
             fetch(`${Url()}/api/becas`, requestOption)
-              .then(r => r.json())
-              .then(r => {
-                console.log(r)
-                setBecas(r);
-                toast.dismiss(toastLoading);
-              })
-              .catch(console.log)
+                .then(r => r.json())
+                .then(r => {
+                    console.log(r)
+                    setBecas(r);
+                    toast.dismiss(toastLoading);
+                })
+                .catch(console.log)
         }
     }, [])
 
     const seleccionarBeca = (idBeca) => {
-        setBecaSeleccionada(becas.filter(b => b.id === idBeca)[0]); 
+        setBecaSeleccionada(becas.filter(b => b.id === idBeca)[0]);
         setModal("flex");
     };
 
@@ -55,18 +55,19 @@ const Becas = () => {
         <div>
             <Toaster />
 
-        <div id='becas'>
+            <div id='becas'>
 
-            <div id='becas-titulo'>
-                <h2>Becas</h2>
+                <div id='becas-titulo'>
+                    <h2>Becas</h2>
+                </div>
+                <div id='lista-becas'>
+                    {becas.map(b => <CardBeca key={b.id} idBeca={b.id} titulo={b.nombreBeca} tags={b.tags} click={seleccionarBeca} />)}
+                </div>
+                <ModalBeca mostrar={modal} setMostrar={setModal} beca={becaSelecionada} />
             </div>
-            <div id='lista-becas'>
-                {becas.map(b => <CardBeca key={b.id} idBeca={b.id} titulo={b.nombreBeca} tags={b.tags} click={seleccionarBeca} />)}
-            </div>
-            <ModalBeca mostrar={modal} setMostrar={setModal} beca={becaSelecionada}/>
         </div>
-        
-    </>
-}
 
-export default Becas
+        </>
+    }
+
+export default Becas;
